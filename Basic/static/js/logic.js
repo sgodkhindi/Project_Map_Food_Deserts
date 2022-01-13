d9076da3b3c81b8f479c593478b1e11075713cb3
sgodkhindi <godkhindis@gmail.com>
2022-01-13 01:01:14 -0500

    Changes to HTML and JS Code 1/12/2022

--- Basic/static/js/logic.js
+++ Basic/static/js/logic.js
@@ -1,58 +1,67 @@
-function createMap(FastFoodStations) {
 
-  // Create the tile layer that will be the background of our map.
-  var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  });
-
-
-  // Create a baseMaps object to hold the streetmap layer.
-  var baseMaps = {
-    "Street Map": streetmap
-  };
-
-  // Create an overlayMaps object to hold the bikeStations layer.
-  var overlayMaps = {
-    "Fast Food Locations": FastFoodStations
-  };
-
-  // Create the map object with options.
-  var map = L.map("map-id", {
-    center: [33.7490, -84.3880],
-    zoom: 12,
-    layers: [streetmap, FastFoodStations]
-  });
-
-  // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(map);
-}
-
-function createMarkersFF(response) {
+function createMarkers(response,style) {
 
   // Pull the "stations" property from response.data.
   var stations = response //.data.stations;
-    // console.log(stations)
+  // console.log(stations)
   // Initialize an array to hold bike markers.
-  var FastFoodMarkers = [];
+  var StoreMarkers = [];
 
   // Loop through the stations array.
   for (var index = 0; index < stations.length; index++) {
     var station = stations[index];
     // console.log(station)
     // For each station, create a marker, and bind a popup with the station's name.
-    var FastFoodMarker = L.marker([station.Store_Lat, station.Store_Long])
-      .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
+    var StoreMarker = L.marker([station.Store_Lat, station.Store_Long], style)
+      .bindPopup("<h3>" + station.Store_Name + "<h3><h3>County: " + station.County_Name + "</h3>");
 
     // Add the marker to the bikeMarkers array.
-    FastFoodMarkers.push(FastFoodMarker);
+    StoreMarkers.push(StoreMarker);
   }
-
+  // console.log(StoreMarkers)
   // Create a layer group that's made from the bike markers array, and pass it to the createMap function.
-  createMap(L.layerGroup(FastFoodMarkers));
+  // createMap(L.layerGroup(FastFoodMarkers));
+  return L.layerGroup(StoreMarkers)
 }
 
-// Perform an API call to the Citi Bike API to get the station information. Call createMarkers when it completes.
-d3.json("resources/county_fast_food_final.json").then(createMarkersFF);
+// function createMarkersFF(response) {
 
+// Perform an API call to the Citi Bike API to get the station information. Call createMarkers when it completes.
+// d3.json("resources/county_fast_food_final.json").then(createMarkersFF);
+d3.json("resources/county_fast_food_final.json").then(function (ffdata) {
+  d3.json("resources/county_dollar_stores_final.json").then(function (dsdata) {
+  d3.json("resources/county_grocery_stores_final.json").then(function (grocerydata) {
+    // Create the tile layer that will be the background of our map.
+    var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+    // console.log(ffdata,grocerydata)
+    let FFLayer = createMarkers(ffdata,{stroke: false, color: "purple", fillColor: "purple"})
+    let DSLayer = createMarkers(dsdata,{stroke: false, color: "blue", fillColor: "blue"})
+    let GroceryLayer = createMarkers(grocerydata,{stroke: false, color: "green", fillColor: "green"})
+    // Create a baseMaps object to hold the streetmap layer.
+    var baseMaps = {
+      "Street Map": streetmap
+    };
+    
+    
+    // Create an overlayMaps object to hold the bikeStations layer.
+    var overlayMaps = {
+      "Fast Food Locations": FFLayer,
+      "Grocery Store Locations": GroceryLayer,
+      "Dollar Store Locations": DSLayer
+    };
+    
+    // Create the map object with options.
+    var map = L.map("map-id", {
+      center: [33.7490, -84.3880],
+      zoom: 12,
+      layers: [streetmap, FFLayer]
+    });
+    
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(map);
+  });
+});
+});
