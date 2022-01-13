4fd1e1552c176263d2fe4cb6cfd2ee66d1cce8ad
sgodkhindi <godkhindis@gmail.com>
2022-01-13 18:12:43 -0500

    New Folder SQL Artifacts 1/13/2022

---
+++ SQL/queries.sql
@@ -0,0 +1,10 @@
+--queries
+SELECT * FROM COUNTIES
+SELECT * FROM dollar_store_final
+SELECT * FROM fast_food_stores
+SELECT * FROM ga_counties_schools
+SELECT * FROM schools_final
+SELECT * FROM stores_final
+SELECT * FROM grocery_store
+SELECT * FROM GA_County_Data
+SELECT * FROM county_stores_final
